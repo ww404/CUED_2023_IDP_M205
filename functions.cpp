16b1fe557bedf6834d3b525449a8a49b7b0f97cd
ww404 <ww404@cam.ac.uk>
2023-11-13 11:02:29 +0000

    line tracking update(tested 10/11, only turns 360)

--- functions.cpp
+++ functions.cpp
@@ -1,41 +1,41 @@
-void MoveForward (int val_Left, int val_Right){
-// This is going to be a function that 
-// 1. moves forward while correcting the mistakes by calling TurnRight or TurnLeft
-// 2. detects if there is a blck ahead and if so, calls function Block
+void LineTracking(void){
+    digitalWrite(led, HIGH);
+    int valLeft_1 = digitalRead(leftlinesensorPin); // read left input value
+    int valRight_1 = digitalRead(rightlinesensorPin); // read right input value
 
-
-    Motor_L->run(RELEASE);
-    Motor_R->run(RELEASE);
-
-
-
-    if (valLeft == HIGH) {
-        Motor_L->run(Forward);
-        Motor_R->run(Backward);
+    if (valRight_1 == HIGH) {
         Motor_R->setSpeed(255);
-        Motor_L->spetSpeed(127);
-        Delay(100);
+        Motor_L->setSpeed(127*0.54);
 
-        //Again modify the delay as required to achieve a suitable amount of rotation
+    //Again modify the delay as required to achieve a suitable amount of rotation
     }
 
-    else if (valRight == HIGH) {
-        Motor_L->run(Forward);
-        Motor_R->run(Backward);
-        Motor_L->setSpeed(255);
-        Motor_R->spetSpeed(127);
-        Delay(100);
+    else if (valLeft_1 == HIGH) {
+        Motor_L->setSpeed(255*0.54);
+        Motor_R->setSpeed(127);
 
-        // Same as above
+
+    // Same as above
     }
 
     else {
-        Motor_L->run(Forward);
-        Motor_R->run(Backward);
-        Motor_L->setSpeed(255);
-        Motor_R->spetSpeed(255);
+        Motor_L->run(BACKWARD);
+        Motor_R->run(FORWARD);
+        Motor_L->setSpeed(255*0.54);
+        Motor_R->setSpeed(255);
     }
+}
 
+void MoveForward (void){
+// This is going to be a function that 
+// 1. moves forward while correcting the mistakes by calling TurnRight or TurnLeft
+// 2. detects if there is a blck ahead and if so, calls function Block
+    while (Found_Block == false){
+        dist_t = sensity_t * MAX_RANG / ADC_SOLUTION ;
+        Found_Block = (dist_t < 5);
+        LineTracking();
+    }
+    Block();
 }
 
 // if angles = 90, then it turns exactly 90 degrees to the left or right
@@ -77,12 +77,11 @@ void Reverse (){
 
 void Block (int val_Ultra, int val_Mag){
 
-    // Detecting a 
     // Detects if the block is magnetic or not
     // Flashes the LED light
     // Clamps the clamp 
 
-
+    Magnetic = digitalRead(MagInputPin);
 
 }
  
--- line_tracker.cpp
+++ line_tracker.cpp
@@ -1,35 +1,24 @@
-/* Notes to Bram: pins select using only numbers are digital pins, pins selected using A followed by a number are analogue pins
-Void before a function basically tell the program to not run the function at that point, allowing you to define a function for use later
-bool left defines left as a boolian function that can only be true or false.
-
-*/
 
 // From the motor sample code, pre-setup
 #include <Adafruit_MotorShield.h>
 
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-// Or, create it with a different I2C address (say for stacking)
-// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61);
-
-// Select which 'port' M1, M2, M3 or M4. In this case, M1
 Adafruit_DCMotor *Motor_L = AFMS.getMotor(1);
-// You can also make another motor on port M2
-Adafruit_DCMotor Motor_R = AFMS.getMotor(2);
+Adafruit_DCMotor *Motor_R = AFMS.getMotor(2);
 
 
 
 
 // Function: track white lines and turn or keep following the line when there is a intersection
-
-
-int leftlinesensorPin = 2; // Connect left line sensor to input pin 2
-int rightlinesensorPin = 3; // Connect right line sensor to input pin 3
+int led = 2;
+int leftlinesensorPin = 3; // Connect left line sensor to input pin 2
+int rightlinesensorPin = 4; // Connect right line sensor to input pin 3
+int centerlinesensorPin = 5;
 
 
 // This is a function telling it to move forward until the line detector notices something different
 
-// Moving forward??? --> I'm (Bram) going to assume you mean left
 void TurnLeft (bool left){
     while (left) {
 
@@ -42,8 +31,6 @@ void TurnLeft (bool left){
     delay(10);
 }
 
-
-// Turning left??? --> I'm (Bram) going to assume you mean forward
 void MoveForward (bool stop){
     while (!stop) {
         // motor speeds not corrected for the different running speeds on the motors on the current prototype (#1)
@@ -63,7 +50,8 @@ void setup() {
 Serial.begin(9600); // Init the serial port
 pinMode(leftlinesensorPin, INPUT); // declares left linesensor as an input
 pinMode(rightlinesensorPin, INPUT); // declares right linesensor as an input
-
+pinMode(centerlinesensorPin, INPUT); 
+pinMode(led, OUTPUT);
 
 
 
@@ -98,56 +86,42 @@ pinMode(rightlinesensorPin, INPUT); // declares right linesensor as an input
 
 void loop(){
 
-// Commented out extra input readings as I (Bram) don't think they are needed, i.e each sensor only has one input pin.
 // Also correct for the previous change made witht regard to sensor input by removing the _1 from both "active sensors"
 
-
+digitalWrite(led, HIGH);
 int valLeft_1 = digitalRead(leftlinesensorPin); // read left input value
-//int valLeft_2 = digitalRead(leftlinesensorPin_2); 
-// Serial.print(valLeft_1);
 int valRight_1 = digitalRead(rightlinesensorPin); // read right input value
-//int valRight_2 = digitalRead(rightlinesensorPin_2); 
+int valCenter = digitalRead(centerlinesensorPin); 
 
+while (valCenter == LOW){
 
-// Serial.println(valRight_1); delay(100); // prinln starts a new line after the output
-
-
-/*
-I'm (Bram) not entirely sure about this, it's probably coirrect but I've written an alternative below so wehave two things to test
-
-stop = (! valRight_2 == valRight_1)
-turn_left = (valRight_2 == HIGH)
-
-if (valRight_2 == valRight_1) {
-    //keep going straight
-    MoveForward(stop)
-}
-else if (valRight_1 == HIGH) {
-    //turn left 90 degrees and then go straight
-    TurnLeft(turn_left)
-}
-
-*/
-
-
-if (valLeft_1 == HIGH) {
-    Motor_R->setSpeed(255)
-    Motor_L->spetSpeed(127)
-    Delay(10)
+if (valRight_1 == HIGH) {
+    Motor_R->setSpeed(255);
+    Motor_L->setSpeed(127*0.54);
 
     //Again modify the delay as required to achieve a suitable amount of rotation
 }
 
-if (valLeft_2 == HIGH) {
-    Motor_L->setSpeed(255)
-    Motor_R->spetSpeed(127)
-    Delay(10)
+else if (valLeft_1 == HIGH) {
+    Motor_L->setSpeed(255*0.54);
+    Motor_R->setSpeed(127);
+
 
     // Same as above
 }
 
 else {
-    MoveForward(stop)
+        Motor_L->run(BACKWARD);
+        Motor_R->run(FORWARD);
+        Motor_L->setSpeed(255*0.54);
+        Motor_R->setSpeed(255);
 }
 
+}
+
+  Motor_L->run(FORWARD);
+  Motor_R->run(FORWARD);
+  Motor_L->setSpeed(255*0.54);
+  Motor_R->setSpeed(255);
+  delay(1850); 
 }
